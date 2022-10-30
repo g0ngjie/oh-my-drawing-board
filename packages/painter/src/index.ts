0c59e7acf9fd59262bd73a2416d639642556d3fd
Gj <gongjie0422@163.com>
2022-10-30 20:04:40 +0800

    feat: lib

--- packages/painter/src/index.ts
+++ packages/painter/src/index.ts
@@ -2,71 +2,74 @@ import styl from "./index.module.css"
 import { Colors as injectColors, Clean as injectClean } from "./tools/index";
 import { isMobile } from "./utils/index";
 
-const canvas = document.createElement("canvas")
+export default function (container: HTMLElement) {
+
+    const canvas = document.createElement("canvas")
 
 
-canvas.height = document.documentElement.clientHeight;
-canvas.width = document.documentElement.clientWidth;
-canvas.className = styl.container
+    canvas.height = document.documentElement.clientHeight;
+    canvas.width = document.documentElement.clientWidth;
+    canvas.className = styl.container
 
 
-let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-ctx.lineWidth = 4;
-ctx.lineCap = "round";
-ctx.strokeStyle = "#f52443";
-let isDrawing = false;
-let last: number[];
+    let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.lineWidth = 4;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#f52443";
+    let isDrawing = false;
+    let last: number[];
 
-injectColors(ctx)
-injectClean(ctx, canvas)
+    injectColors(ctx)
+    injectClean(ctx, canvas)
 
-if (isMobile) {
-    canvas.ontouchstart = (e) => {
-        last = [e.touches[0].clientX, e.touches[0].clientY];
-    };
-    canvas.ontouchmove = (e) => {
-        drawLine(last[0], last[1], e.touches[0].clientX, e.touches[0].clientY);
-        last = [e.touches[0].clientX, e.touches[0].clientY];
-    };
-} else {
-    canvas.onpointermove = (e: PointerEvent) => {
-        ctx.lineWidth = getLineWidth(e);
-        if (isDrawing) {
-            drawLine(last[0], last[1], e.clientX, e.clientY);
-            last = [e.clientX, e.clientY];
-        }
-    };
-    canvas.onpointerdown = (e) => {
-        isDrawing = true;
-        last = [e.clientX, e.clientY];
-    };
-    canvas.onpointerup = () => {
-        isDrawing = false;
-    };
-}
-
-
-function drawLine(x1: number, y1: number, x2: number, y2: number) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-}
-
-function getLineWidth(e: PointerEvent) {
-    switch (e.pointerType) {
-        case "touch": {
-            if (e.width < 10 && e.height < 10) {
-                return (e.width + e.height) * 2 + 10;
-            } else {
-                return (e.width + e.height - 40) / 2;
+    if (isMobile) {
+        canvas.ontouchstart = (e) => {
+            last = [e.touches[0].clientX, e.touches[0].clientY];
+        };
+        canvas.ontouchmove = (e) => {
+            drawLine(last[0], last[1], e.touches[0].clientX, e.touches[0].clientY);
+            last = [e.touches[0].clientX, e.touches[0].clientY];
+        };
+    } else {
+        canvas.onpointermove = (e: PointerEvent) => {
+            ctx.lineWidth = getLineWidth(e);
+            if (isDrawing) {
+                drawLine(last[0], last[1], e.clientX, e.clientY);
+                last = [e.clientX, e.clientY];
             }
-        }
-        case "pen":
-            return e.pressure * 8;
-        default:
-            return e.pressure ? e.pressure * 8 : 4;
+        };
+        canvas.onpointerdown = (e) => {
+            isDrawing = true;
+            last = [e.clientX, e.clientY];
+        };
+        canvas.onpointerup = () => {
+            isDrawing = false;
+        };
     }
-}
 
-document.documentElement.appendChild(canvas)
+
+    function drawLine(x1: number, y1: number, x2: number, y2: number) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+    }
+
+    function getLineWidth(e: PointerEvent) {
+        switch (e.pointerType) {
+            case "touch": {
+                if (e.width < 10 && e.height < 10) {
+                    return (e.width + e.height) * 2 + 10;
+                } else {
+                    return (e.width + e.height - 40) / 2;
+                }
+            }
+            case "pen":
+                return e.pressure * 8;
+            default:
+                return e.pressure ? e.pressure * 8 : 4;
+        }
+    }
+
+    container.appendChild(canvas)
+}
---
+++ packages/vuepage/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import draw from "@drawing/painter";
+import "@drawing/painter/lib/style.css";
+
+const container = ref();
+onMounted(() => {
+  console.log("[debug]123:", 123)
+  console.log("[debug]container.value:", container.value)
+  // createDraw(container.value)
+  console.log("[debug]draw:", draw)
+  // console.log("[debug]createDraw:", createDraw)
+})
+</script>
+
+<template>
+  <div ref="container" class="w-100% h-100% bg-light-50">
+    asdf
+  </div>
+</template>
